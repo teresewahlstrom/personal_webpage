26420149e73b07c2aa77f466172e670c5f2f832a
Terese Wahlström <terese.wahlstrom@gmail.com>
2026-05-23 16:38:02 +0200

    main app streamlinings

--- lib/modals/newsletter/embed_web.dart
+++ lib/modals/newsletter/embed_web.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: avoid_web_libraries_in_flutter, deprecated_member_use
-
-import 'dart:html' as html;
 import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/material.dart';
+import 'package:web/web.dart' as web;
 
 import 'embed_config.dart';
 
@@ -18,7 +16,7 @@ void _registerViewFactoryIfNeeded() {
   ui_web.platformViewRegistry.registerViewFactory(_newsletterViewType, (
     int viewId,
   ) {
-    final html.IFrameElement iframe = html.IFrameElement()
+    final web.HTMLIFrameElement iframe = web.HTMLIFrameElement()
       ..src =
           "https://sibforms.com/serve/MUIFALacijj-Gm_7_5NdvnOGr3ECgmv2aMTsHl6lF7cGsIwdtxewz1YRowtenm7DKATFzm-OEC3Vi8ULPriXsjl9DNNIY3uAxR4hkKDKk2RagWMLodj1FHTbVlWVRMx9W8QGdodlpNJvk4tnxWLXpfVhpqQf6M-7RZEIJoDb9KKFkPGHzsROhNdcRF6EF728A01BtHw_TsBN3MG2"
       ..style.border = "0"
